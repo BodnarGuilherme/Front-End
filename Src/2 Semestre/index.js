7b819818c16a337e9d2d78282c61403dcce3a5a2
Guilherme <82243438+BodnarGuilherme@users.noreply.github.com>
2021-08-18 11:14:50 -0300

    Delete index.js

--- Src/2 Semestre/index.js
+++
@@ -1,8 +0,0 @@
-function calcularImc(){
-    var peso = document.getElementById("Peso").value
-    var altura = document.getElementById("Altura").value
-    var imc = peso / (altura * altura)
-    console.log(imc)
-
-    alert("Seu imc é: " + Math.round(imc))
-}
