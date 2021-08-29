e2c07dc7a2766ebe7c03b49f04e162d91afc5b23
Guilherme <82243438+BodnarGuilherme@users.noreply.github.com>
2021-08-28 23:20:04 -0300

    Add files via upload

--- Src/2 Semestre/script.js
+++ Src/2 Semestre/script.js
@@ -1,8 +1,28 @@
-function calcularImc(){
-    var peso = document.getElementById("Peso").value
-    var altura = document.getElementById("Altura").value
-    var imc = peso / (altura * altura)
-    console.log(imc)
+function classificarImc(){
+    var img = document.getElementById("img");
+    var peso = document.getElementById("Peso").value;
+    var altura = document.getElementById("Altura").value;
+    var imc = (peso / (altura * altura)).toFixed(1);
+    var res = document.getElementById("res");
 
-    alert("Seu imc é: " + Math.round(imc))
+    if(imc <= 18.5){
+        img.src = "https://abeso.org.br/wp-content/uploads/2019/12/imc_06.png";
+        res.innerText = "Seu imc: " + imc + " - Abaixo do normal";
+    }else if(imc <= 24.9){
+        img.src = "https://abeso.org.br/wp-content/uploads/2019/12/imc_05.png";
+        res.innerText = "Seu imc: " + imc + " - Normal";
+    }else if(imc <= 29.9){
+        img.src = "https://abeso.org.br/wp-content/uploads/2019/12/imc_04.png";
+        res.innerText = "Seu imc: " + imc + " - Sobrepeso";
+    }else if(imc <= 34.9){
+        img.src = "https://abeso.org.br/wp-content/uploads/2019/12/imc_03.png";
+        res.innerText = "Seu imc: " + imc + " - Obesidade grau I";
+    }else if(imc <= 39.9){
+        img.src = "https://abeso.org.br/wp-content/uploads/2019/12/imc_02.png";
+        res.innerText = "Seu imc: " + imc + " - Obesidade grau II";
+    }else{
+        img.src = "https://abeso.org.br/wp-content/uploads/2019/12/imc_01.png";
+        res.innerText = "Seu imc: " + imc + " - Obesidade grau III";
+    }
+
 }
